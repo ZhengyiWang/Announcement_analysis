8f8abd9036289c727a419d7399923f7e8f3aa890
ZhengyiWang <wbswzy@163.com>
2024-03-21 13:09:20 +0800

    【金融财经】- 书生·浦语大模型在公告理解的应用挑战

---
+++ server/agent/tools_select.py
@@ -0,0 +1,19 @@
+from langchain.tools import Tool
+from server.agent.tools import *
+
+tools = [
+    Tool.from_function(
+        func=search_knowledgebase_complex,
+        name="search_knowledgebase_complex",
+        description="Use Use this tool to search local knowledgebase and get information",
+        args_schema=KnowledgeSearchInput,
+    ),
+    Tool.from_function(
+        func=search_internet,
+        name="search_internet",
+        description="Use this tool to use bing search engine to search the internet",
+        args_schema=SearchInternetInput,
+    )
+]
+
+tool_names = [tool.name for tool in tools]
